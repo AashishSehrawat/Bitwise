f6117fdd55f433bd4c19fbbac0f0f4b1c2f82735
AashishSehrawat <aashishsehrawat98174@gmail.com>
2023-08-28 23:24:19 +0530

    Bitwiseoperator added

---
+++ BitwiseOperator.java
@@ -0,0 +1,13 @@
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        int p =9;
+        int q=10;
+        System.out.println(p|q);
+        System.out.println(p&q);
+        System.out.println(p^q);
+        System.out.println(~p);
+        System.out.println(p<<1);
+        System.out.println(p>>1);
+    
+}
+}
